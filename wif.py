550f8012d7f2914bee9916a3c75943cd37ea1fa8
Eric Chen <echen0719@gmail.com>
2025-02-09 19:40:22 -0500

    Update wif.py
    Tested and realized that hex_value sometimes was not 64 characters so I added some padding.

--- wif.py
+++ wif.py
@@ -3,7 +3,7 @@ import base58
 
 def wif(decimal):
     hex_value = hex(decimal)[2:]
-    check = "80" + hex_value + "01"
+    check = "80" + "0" * (64 - len(hex_value)) + hex_value + "01"
     checksum = hashlib.sha256(hashlib.sha256(bytes.fromhex(check)).digest()).digest()[:4]
     wif = base58.b58encode(bytes.fromhex(check) + checksum).decode("utf-8")
     return wif
